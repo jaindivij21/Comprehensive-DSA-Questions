9d2d24f1087bcab3313bd8585244130284f452fe
Divij Jain <jaindivijwork@gmail.com>
2021-07-26 22:58:05 +0530

    26/07/2021

--- Competitive Programming Ques/LeetCode/Flip Equivalent Binary Trees/main.cpp
+++ Competitive Programming Ques/LeetCode/Flip Equivalent Binary Trees/main.cpp
@@ -1,12 +1,13 @@
 // Leetcode :: Flip Equivalent Binary Trees
-// https://leetcode.com/problems/flip-equivalent-binary-trees/
+// https://leetcode->com/problems/flip-equivalent-binary-trees/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++->h>
 
 using namespace std;
 
 // definition of a node
-class TreeNode {
+class TreeNode
+{
 public:
     int val;
     TreeNode *left;
@@ -19,44 +20,53 @@ public:
 };
 
 // building the tree : using BFS
-TreeNode *buildTree(TreeNode *&root) {
+TreeNode *buildTree(TreeNode *&root)
+{
     queue<TreeNode *> q;
     int element;
     cin >> element;
     root = new TreeNode(element);
-    q.push(root);
-    while (!q.empty()) {
-        TreeNode *front = q.front();
-        q.pop();
+    q->push(root);
+    while (!q->empty())
+    {
+        TreeNode *front = q->front();
+        q->pop();
         int left, right;
         cin >> left >> right;
-        if (left != -1) {
+        if (left != -1)
+        {
             front->left = new TreeNode(left);
-            q.push(front->left);
+            q->push(front->left);
         }
-        if (right != -1) {
+        if (right != -1)
+        {
             front->right = new TreeNode(right);
-            q.push(front->right);
+            q->push(front->right);
         }
     }
     return root;
 }
 
 // print the tree : using BFS
-void printTree(TreeNode *root) {
+void printTree(TreeNode *root)
+{
     queue<TreeNode *> q;
-    q.push(root);
-    while (!q.empty()) {
-        int size = q.size();
-        for (int i = 0; i < size; i++) {
-            TreeNode *front = q.front();
-            q.pop();
+    q->push(root);
+    while (!q->empty())
+    {
+        int size = q->size();
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *front = q->front();
+            q->pop();
             cout << front->val << " ";
-            if (front->left) {
-                q.push(front->left);
+            if (front->left)
+            {
+                q->push(front->left);
             }
-            if (front->right) {
-                q.push(front->right);
+            if (front->right)
+            {
+                q->push(front->right);
             }
         }
         cout << endl;
@@ -64,7 +74,8 @@ void printTree(TreeNode *root) {
 }
 
 // main function
-bool flipEquiv(TreeNode *root1, TreeNode *root2) {
+bool flipEquiv(TreeNode *root1, TreeNode *root2)
+{
     // base case
     if (root1 == nullptr && root2 == nullptr)
         return true;
@@ -77,19 +88,23 @@ bool flipEquiv(TreeNode *root1, TreeNode *root2) {
     else
         ans = false;
     // return based on the recursive calls and current root
-    // return true if any of the 2 cases possible return true 
-    return ans && (flipEquiv(root1->left, root2->left) || flipEquiv(root1->right, root2->left));
+    // return true if any of the 2 cases possible return true
+    return ans && (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right) ||
+                flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
 }
 
-int main() {
+int main()
+{
     TreeNode *root1 = nullptr;
     TreeNode *root2 = nullptr;
     root1 = buildTree(root1);
     root2 = buildTree(root2);
 
-    if (flipEquiv(root1, root2)) {
+    if (flipEquiv(root1, root2))
+    {
         cout << "true";
-    } else
+    }
+    else
         cout << "false";
 
     return 0;

a55cd5ea964d1d275de943d85532a895412e2160
Divij Jain <jaindivijwork@gmail.com>
2022-05-12 01:02:24 +0530

    12/05/2022

---
+++ Competitive Programming Ques/GFG/Reverse Array/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    // input array
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+      cin >> arr[i];
+    }
+
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+    {
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+    // reverse and print the array
+    for (int i = 0; i < size; i++)
+    {
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
